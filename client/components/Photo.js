23d298c0714821794f83992528a091dcdb4dc8fe
Addy <Ashepherdking@gmail.com>
2017-11-14 13:58:37 -0700

    connected the like and post reducers with the actions to update

--- client/components/Photo.js
+++ client/components/Photo.js
@@ -22,7 +22,22 @@ const Photo = React.createClass({
           </CSSTransitionGroup>
 
         </div>
-        
+
+        <figcaption>
+          <p>{post.caption}</p>
+          <div className='control-buttons'>
+            <button onClick={this.props.increment.bind(null, i)} className='likes'>
+              &hearts; {post.likes}
+            </button>
+            <Link className='button' to={`/view/${post.code}`}>
+              <span className='comment-count'>
+                <span className='speech-bubble'></span>
+                {comments[post.code] ? comments[post.code].length : 0 }
+              </span>
+            </Link>
+          </div>
+        </figcaption>
+
       </figure>
     )
   }
