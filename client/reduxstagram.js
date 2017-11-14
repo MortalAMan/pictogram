8872ae5e8a69c56f9f3763980611fec918e9ddd5
Addy <Ashepherdking@gmail.com>
2017-11-13 17:41:19 -0700

    stubbed out Single/PhotoGrid components

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,1 +1,8 @@
 import React from 'react';
+
+import { render } from 'react-dom';
+import Main from './components/Main';
+
+import css from './styles/style.styl';
+
+render(<Main/>, document.getElementById('root'));
