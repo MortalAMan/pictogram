a0e7932cb33eb4183b0596156384c491f0ef5883
Addy <Ashepherdking@gmail.com>
2017-11-14 01:55:39 -0700

    connected state data to components PhotoGrid/Photo/App/Main

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Photo from './Photo';
 
 class PhotoGrid extends React.Component {
   render () {
-    return <div className='photo-grid'>Photo Grid</div>
+    return <div className='photo-grid'>
+      {this.props.posts.map((post, i) => <Photo {...
+        this.props} key={i} i={i} post={post} />)}
+    </div>
   }
 }
 
--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,11 +1,6 @@
-// a reducer takes in 2 things:
-
-// 1. the action (info about what happened)
-// 2. copy of current state
-
-function posts(state = [], action) {
+function comments(state = [], action) {
   console.log(state, action);
   return state;
 }
 
-export default posts;
+export default comments;
